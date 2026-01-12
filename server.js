2c99438836c8d6a4b68534cad634ac372c6ef1da
unknowne1931 <unknowne1931@gmail.com>
2026-01-12 23:54:36 +0530

    Anu

--- server.js
+++ server.js
@@ -213,7 +213,7 @@ app.post(
 
 
 app.use(cors({
-    origin: "*",
+    origin: ["https://stawro.com", "https://www.stawro.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
@@ -390,7 +390,7 @@ export async function get_per(user) {
 }
 
 app.get('/', (req, res) => {
-    res.send('Hello, world Vs : 5.1.0 ; Last Updated : 12-01-2026 ; Type : Live');
+    res.send('Hello, world Vs : 5.1.1 ; Last Updated : 12-01-2026 ; Type : Live');
 });
 
 
